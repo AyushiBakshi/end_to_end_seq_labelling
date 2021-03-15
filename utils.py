4405757e01522e31bf199bb23464123a64888dd7
ayushibakshi <ayushibakshi16@gmail.com>
2021-03-15 14:11:59 +0800

    Code base setup

---
+++ utils.py
@@ -0,0 +1,135 @@
+import torch
+#Constants
+START_TAG = '<START>'
+STOP_TAG = '<STOP>'
+
+def lower_case(x,lower=False):
+    if lower:
+        return x.lower()
+    else:
+        return x
+
+
+def log_sum_exp(vec):
+    '''
+    This function calculates the score explained above for the forward algorithm
+    vec 2D: 1 * tagset_size
+    '''
+    max_score = vec[0, argmax(vec)]
+    max_score_broadcast = max_score.view(1, -1).expand(1, vec.size()[1])
+    return max_score + torch.log(torch.sum(torch.exp(vec - max_score_broadcast)))
+
+
+def argmax(vec):
+    '''
+    This function returns the max index in a vector
+    '''
+    _, idx = torch.max(vec, 1)
+    return to_scalar(idx)
+
+
+def to_scalar(var):
+    '''
+    Function to convert pytorch tensor to a scalar
+    '''
+    return var.view(-1).data.tolist()[0]
+
+def score_sentences(self, feats, tags):
+    # tags is ground_truth, a list of ints, length is len(sentence)
+    # feats is a 2D tensor, len(sentence) * tagset_size
+    r = torch.LongTensor(range(feats.size()[0]))
+    if self.use_gpu:
+        r = r.cuda()
+        pad_start_tags = torch.cat([torch.cuda.LongTensor([self.tag_to_ix[START_TAG]]), tags])
+        pad_stop_tags = torch.cat([tags, torch.cuda.LongTensor([self.tag_to_ix[STOP_TAG]])])
+    else:
+        pad_start_tags = torch.cat([torch.LongTensor([self.tag_to_ix[START_TAG]]), tags])
+        pad_stop_tags = torch.cat([tags, torch.LongTensor([self.tag_to_ix[STOP_TAG]])])
+
+    score = torch.sum(self.transitions[pad_stop_tags, pad_start_tags]) + torch.sum(feats[r, tags])
+
+    return score
+
+
+def get_chunk_type(tok, idx_to_tag):
+    """
+    The function takes in a chunk ("B-PER") and then splits it into the tag (PER) and its class (B)
+    as defined in BIOES
+
+    Args:
+        tok: id of token, ex 4
+        idx_to_tag: dictionary {4: "B-PER", ...}
+
+    Returns:
+        tuple: "B", "PER"
+
+    """
+
+    tag_name = idx_to_tag[tok]
+    tag_class = tag_name.split('-')[0]
+    tag_type = tag_name.split('-')[-1]
+    return tag_class, tag_type
+
+
+def get_chunks(seq, tags):
+    """Given a sequence of tags, group entities and their position
+
+    Args:
+        seq: [4, 4, 0, 0, ...] sequence of labels
+        tags: dict["O"] = 4
+
+    Returns:
+        list of (chunk_type, chunk_start, chunk_end)
+
+    Example:
+        seq = [4, 5, 0, 3]
+        tags = {"B-PER": 4, "I-PER": 5, "B-LOC": 3}
+        result = [("PER", 0, 2), ("LOC", 3, 4)]
+
+    """
+
+    # We assume by default the tags lie outside a named entity
+    default = tags["O"]
+
+    idx_to_tag = {idx: tag for tag, idx in tags.items()}
+
+    chunks = []
+
+    chunk_type, chunk_start = None, None
+    for i, tok in enumerate(seq):
+        # End of a chunk 1
+        if tok == default and chunk_type is not None:
+            # Add a chunk.
+            chunk = (chunk_type, chunk_start, i)
+            chunks.append(chunk)
+            chunk_type, chunk_start = None, None
+
+        # End of a chunk + start of a chunk!
+        elif tok != default:
+            tok_chunk_class, tok_chunk_type = get_chunk_type(tok, idx_to_tag)
+            if chunk_type is None:
+                # Initialize chunk for each entity
+                chunk_type, chunk_start = tok_chunk_type, i
+            elif tok_chunk_type != chunk_type or tok_chunk_class == "B":
+                # If chunk class is B, i.e., its a beginning of a new named entity
+                # or, if the chunk type is different from the previous one, then we
+                # start labelling it as a new entity
+                chunk = (chunk_type, chunk_start, i)
+                chunks.append(chunk)
+                chunk_type, chunk_start = tok_chunk_type, i
+        else:
+            pass
+
+    # end condition
+    if chunk_type is not None:
+        chunk = (chunk_type, chunk_start, len(seq))
+        chunks.append(chunk)
+
+    return chunks
+
+def adjust_learning_rate(optimizer, lr):
+    """
+    shrink learning rate
+    """
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
